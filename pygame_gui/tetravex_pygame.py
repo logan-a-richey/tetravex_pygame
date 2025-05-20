2441b948cd86bd2fbc47fb695556660adb78befb
logan-a-richey <loganricheygithub@gmail.com>
2025-05-20 16:59:08 -0500

    better but grid coordinates are broken

--- pygame_gui/tetravex_pygame.py
+++ pygame_gui/tetravex_pygame.py
@@ -6,146 +6,184 @@ import random
 import time
 random.seed(time.time())
 
-from typing import List, Dict
-from copy import deepcopy
-
-class Tile:
-    def __init__(self, game, i, j, n, e, s, w):
-        self.game = game
-        self.i = i
-        self.j = j
-
-        self.n = n
-        self.e = e
-        self.s = s
-        self.w = w
-
-    def update(self):
-        # TODO animation
-        return
-
-    def draw(self):
-        # TODO draw triangles
-
-        # TODO draw numbers
-
-        return
-
-class Tetravex:
-    def __init__(self, game, board_size=3):
-        self.board_size = board_size
-        self.board = [[None for i in range(board_size)] for j in range(board_size*2)]
-        
-        for i in range(board_size):
-            for j in range(board_size):
-                n, e, s, w = [random.randint(0,9) for _ in range(4)]
-                self.board[i][j] = Tile(game, i, j, n, e, s, w)
-                t = self.board[i][j]
-
-                # compare up
-                if i > 0:
-                    t.n = self.board[i-1][j].s
-                # compare left
-                if j > 0:
-                    t.w = self.board[i][j-1].e
-        
-    def make_move(self, ai, aj, bi, bj):
-        # get grids
-        g1 = self.board[ai][aj]
-        g2 = self.board[bi][bj]
-        
-        # set internal coordinates
-        if isinstance(g1, Tile):
-            g1.i = bi
-            g1.j = bj
-        if isinstance(g2, Tile):
-            g2.i = ai
-            g2.j = aj
-
-        # swap tiles a and b
-        g1, g2 = g2, g1
-
-        # TODO check solved state
-        return
-
-class Square:
-    def __init__(self, game, grid=0, i=0, j=0, color=(100, 100, 100)):
-        self.game = game
-        self.grid = grid
-        self.i = i
-        self.j = j
-        self.color = color
-        self.border_color = (0, 0, 0)
-        
-        # rect: x, y, width, height
-        self.rect = pygame.Rect(
-            (j * 100) + grid * (game.board_size*100 + 100), 
-            i * 100, 
-            100, 
-            100
-        )
-
-    def update(self):
-        pass
-
-    def draw(self):
-        pygame.draw.rect(self.game.screen, self.color, self.rect)
-        pygame.draw.rect(self.game.screen, self.border_color, self.rect, width=2)
-
+from pygame_gui.square import Square
+from pygame_gui.tile import Tile
 
 class TetravexPygame:
-    def get_random_color(self):
-        rgb = tuple([random.randint(100,200) for _ in range(3)])
-        return rgb
+    def __init__(self, BOARD_SIZE=3):
+        pygame.init()
 
-    def init_board(self, board_size):
-        self.board_size = board_size
+        self.TILE_SIZE = 100
+        self.SPACING = 50
+        sh = (BOARD_SIZE + 1) * self.TILE_SIZE
+        sw = (BOARD_SIZE * 2 + 1) * self.TILE_SIZE
+
+        self.screen = pygame.display.set_mode((sw, sh))
+        pygame.display.set_caption("Tetravex")
+        self.font = pygame.font.SysFont(None, 36)
+
+        self.running = 0
+        self.last_tile = None
+        self.init_board(BOARD_SIZE)
+
+    def init_board(self, BOARD_SIZE):
+        self.BOARD_SIZE = BOARD_SIZE
+
         self.left_grids = []
         self.right_grids = []
 
-        for i in range(board_size**2):
-            mi = i // self.board_size
-            mj = i % self.board_size
-            rgb = (200,200,200)
+        # Use board as 2D grid [x][y], total width = 2 * BOARD_SIZE
+        self.board = [
+            [None for _ in range(self.BOARD_SIZE)] for _ in range(self.BOARD_SIZE * 2)
+        ]
+
+        self.clicks = []
+        self.is_solved = 0
+
+        for i in range(BOARD_SIZE ** 2):
+            mi = i // BOARD_SIZE
+            mj = i % BOARD_SIZE
+
+            rgb = (200, 200, 200)
 
             sq1 = Square(self, 0, mi, mj, rgb)
             sq2 = Square(self, 1, mi, mj, rgb)
-            
+
+            n, e, s, w = [random.randint(0, 9) for _ in range(4)]
+            tile = Tile(self, mi, mj, n, e, s, w)
+
+            # Match edges with top/left neighbors for solvability
+            if mi > 0:
+                tile.n = self.board[mj][mi - 1].s
+            if mj > 0:
+                tile.w = self.board[mj - 1][mi].e
+
+            tile.i = mi
+            tile.j = mj
+
+            self.board[mj][mi] = tile  # store in left grid only
+
             self.left_grids.append(sq1)
             self.right_grids.append(sq2)
 
+        self.shuffle_tiles()
 
-    def __init__(self, board_size=3):
-        # init pygame
-        pygame.init()
-        sh = (board_size + 1) * 100
-        sw = (board_size * 2 + 1) * 100
+    def shuffle_tiles(self):
+        # Flatten left grid tiles
+        left_tiles = [
+            self.board[x][y]
+            for x in range(self.BOARD_SIZE)
+            for y in range(self.BOARD_SIZE)
+            if self.board[x][y] is not None
+        ]
 
-        self.screen = pygame.display.set_mode( (sw, sh) )
-        pygame.display.set_caption("Tetravex")
-        
-        # init game
-        self.running = 0
-        self.init_board(board_size)
+        # Shuffle them
+        random.shuffle(left_tiles)
+
+        # Put them back and update tile positions
+        idx = 0
+        for x in range(self.BOARD_SIZE):
+            for y in range(self.BOARD_SIZE):
+                tile = left_tiles[idx]
+                self.board[x][y] = tile
+                tile.j = x
+                tile.i = y
+                idx += 1
+
+    def get_coord(self, x, y):
+        my = y // self.TILE_SIZE
+
+        # LEFT grid
+        if 0 <= x < self.BOARD_SIZE * self.TILE_SIZE:
+            mx = x // self.TILE_SIZE
+            if 0 <= my < self.BOARD_SIZE:
+                return (mx, my)
+
+        # RIGHT grid
+        offset = self.BOARD_SIZE * self.TILE_SIZE + self.SPACING  # Replace 100 with self.SPACING if defined
+        if offset <= x < offset + self.BOARD_SIZE * self.TILE_SIZE:
+            mx = (x - offset) // self.TILE_SIZE
+            if 0 <= my < self.BOARD_SIZE:
+                return (mx + self.BOARD_SIZE, my)
+
+        return None
+
+    def make_move(self, ai, aj, bi, bj):
+        tile_a = self.board[ai][aj]
+        tile_b = self.board[bi][bj]
+
+        # Swap tiles in the board
+        self.board[ai][aj], self.board[bi][bj] = tile_b, tile_a
+
+        # Update tile coordinates
+        if tile_a:
+            tile_a.j, tile_a.i = bi, bj
+        if tile_b:
+            tile_b.j, tile_b.i = ai, aj
+
+        res = self.solution_check()
+        if res:
+            self.is_solved = 1
+        else:
+            self.is_solved = 0
+    
+    def solution_check(self):
+        return False
 
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = 0
+
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos=pygame.mouse.get_pos()
-                btn=pygame.mouse
-                print ("x = {}, y = {}".format(pos[0], pos[1]))
-        # TODO add ESC key to also exit game
-        return
+                pos = pygame.mouse.get_pos()
+                res = self.get_coord(pos[0], pos[1])
+                if res:
+                    self.clicks.clear()
+                    self.clicks.append(res)
+                    # grab tile
+                    self.last_tile = self.board[res[0]][res[1]]
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                res = self.get_coord(pos[0], pos[1])
+                if res:
+                    self.clicks.append(res)
+                    if len(self.clicks) == 2:
+                        move = [*self.clicks[0], *self.clicks[1]]
+                        print(move)
+                        self.make_move(*move)
+                
+                self.clicks.clear()
+
+                # release tile
+                self.last_tile = None
+
 
     def draw(self):
         self.screen.fill("white")
 
         for g in self.left_grids:
-            g.draw()      
+            g.draw()
         for g in self.right_grids:
             g.draw()
+
+        # Draw from board
+        for x in range(self.BOARD_SIZE * 2):
+            for y in range(self.BOARD_SIZE):
+                tile = self.board[x][y]
+                if tile:
+                    if tile == self.last_tile:
+                        continue
+                    tile.update()
+                    tile.draw()
+        
+        # Grabbed tile drawn on top, following mouse
+        if self.last_tile:
+            pos = pygame.mouse.get_pos()
+            self.last_tile.x = pos[0] - self.TILE_SIZE // 2
+            self.last_tile.y = pos[1] - self.TILE_SIZE // 2
+            self.last_tile.draw()
 
         pygame.display.flip()
 
